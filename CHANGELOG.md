1af69c5bf8546958b805b91238a1c8c060117b6c
Petter Walbø Johnsgård <petter@dekode.no>
2018-01-31 20:18:50 +0100

    v1.0.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Changelog
 
-## [Unreleased]
+## [1.0.13] - 2018-01-31
 ### Internal
 * Use caption component on caption markup
 * Remove assets enqueue action. Let hogan core handle it.
+* Add frontend setup and minimize assets
--- hogan-embed.php
+++ hogan-embed.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-embed
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-embed
  * Description: oEmbed Module for Hogan
- * Version: 1.0.12
+ * Version: 1.0.13
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0-or-later
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HOGAN_EMBED_VERSION', '1.0.12' );
+define( 'HOGAN_EMBED_VERSION', '1.0.13' );
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\hogan_embed_load_textdomain' );
 add_action( 'hogan/include_modules', __NAMESPACE__ . '\\hogan_embed_register_module' );
